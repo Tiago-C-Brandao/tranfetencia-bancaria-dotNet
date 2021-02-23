5626f9aa7809940f66fadab08a4e709e2304366a
Tiago C. Brandão <tiago_curvelo@hotmail.com>
2021-02-23 18:39:01 -0300

    Finalizando projeto DIO.Bank

--- Program.cs
+++ Program.cs
@@ -23,15 +23,15 @@ namespace DIO.Bank
                         break;
 
                     case "3":
-                        //Transferir();
+                        Transferir();
                         break;
 
                     case "4":
-                        //Sacar();
+                        Sacar();
                         break;
 
                     case "5":
-                        //Depositar();
+                        Depositar();
                         break;
 
                     case "C":
@@ -47,6 +47,41 @@ namespace DIO.Bank
             Console.ReadLine();
         }
 
+        private static void Transferir() 
+        {
+            Console.Write("Digite o número da conta de origem: ");
+            int indiceContaOrigem =  int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número da conta de destino: ");
+            int indiceContaDestino =  int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser transferido: ");
+            double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+        }
+
+        private static void Sacar() 
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta =  int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorSaque);
+        }
+
+        private static void Depositar() 
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta =  int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Depositar(valorDeposito);
+        }
         private static void InserirConta()
         {
             Console.WriteLine("Inserir nova conta");
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Criando uma aplicação de transferências bancárias com .NET
+# Criando uma aplicação de transferências bancárias com .NET 
 
 
 
-Neste projeto aprendi como criar um algoritmo simples de transferência bancária para exercer o pensamento orientado a objetos, o principal paradigma de programação utilizada no mercado. Nesse projeto você vai aprender: Como pensar orientado a objetos, como modelar o seu domínio, como utilizar enums.
+Neste projeto aprendi como criar um algoritmo simples de transferência bancária para exercer o pensamento orientado a objetos, o principal paradigma de programação utilizada no mercado. Aprendi a como pensar **orientado a objetos**, como **modelar o seu domínio**, como **utilizar enums**.
+
+Utilizar o **Visual Studio Code** para programar com **.Net** foi uma experiência bastante produtiva e instigadora por sair da minha zona de conforto usando apenas o **Visual Studio** para programar em **CSharp**, com esse exercício percebi de forma mais profundo o quão versátil é trabalhar com o VS Code independente da linguagem em que se esteja usando. 
